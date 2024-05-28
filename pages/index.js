417b74774847be5e6362ccf66914c177edd1d1be
Bruno Penner <brunopenner01@gmail.com>
2024-05-28 13:28:00 +0800

    #7 BlockRenderer component & clean block data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import { gql } from "@apollo/client";
 import client from "client";
+import { BlockRenderer } from "components/BlockRenderer";
+import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
 
 export default function Home(props) {
   console.log("PROPS", props);
-  return <div>Next JS &amp; WordPress course.</div>;
+  return <div>
+    <BlockRenderer blocks={props.blocks}/>
+  </div>;
 }
 
 export const getStaticProps = async() => {
@@ -21,8 +25,7 @@ export const getStaticProps = async() => {
   })
   return {
     props: {
-      blocks: data.noteByUri.blocks,
-      myexampleprop: "test"
+      blocks: cleanAndTransformBlocks (data.nodeByUri.blocks),
     }
   }
 }
