89b385ff08eb8a5e4f0dca00d84af3e92ac9eb35
Bruno Penner <brunopenner01@gmail.com>
2024-05-29 14:06:25 +0800

    #18 Query and clean menu data in Next JS

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { gql } from "@apollo/client";
 import client from "client";
 import { BlockRenderer } from "components/BlockRenderer";
 import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
+import { mapMainMenuItems } from "utils/mapMainMenuItems";
 
 export default function Home(props) {
   console.log("PROPS", props);
@@ -15,12 +16,34 @@ export default function Home(props) {
 export const getStaticProps = async() => {
   const {data} = await client.query ({
     query: gql`
-      query NewQuery {
+      query PageQuery {
         nodeByUri(uri: "/") {
           ... on Page {
             id
             title
             blocks(postTemplate: false)
+          }
+        }
+        acfOptionsMainMenu {
+          mainMenu {
+            menuItems {
+              menuItem {
+                destination {
+                  ... on Page {
+                    uri
+                  }
+                }
+                label
+              }
+              items {
+                destination {
+                  ... on Page {
+                    uri
+                  }
+                }
+                label
+              }
+            }
           }
         }
       }
@@ -29,6 +52,7 @@ export const getStaticProps = async() => {
   const blocks = cleanAndTransformBlocks (data.nodeByUri.blocks);
   return {
     props: {
+      mainMenuItems: mapMainMenuItems(data.acfOptionsMainMenu.mainMenu.menuItems),
       blocks,
     }
   }
