790e0ea505d3ea48c13b32f1977b303318305459
Bruno Penner <brunopenner01@gmail.com>
2024-06-05 10:32:37 +0800

    22. Refactor Page components

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,46 +1,12 @@
 import { gql } from "@apollo/client";
 import client from "client";
 import { BlockRenderer } from "components/BlockRenderer/BlockRenderer";
-import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
+import { Page } from "components/Page";
+import { getPageStaticProps } from "utils/getPageStaticProps";
 
-export default function Page(props) {
-    console.log("PAGE PROPS: ", props)
-    return (
-        <div>
-            <BlockRenderer blocks={props.blocks}/>
-        </div>
-      )
-}
+export default Page;
 
-export const getStaticProps = async (context) => {
-    console.log("CONTEXT: ", context);
-    const uri = `/${context.params.slug.join("/")}/`
-    console.log("URI: ", uri);
-    const {data} = await client.query ({
-        query: gql`
-          query PageQuery($uri: String!) {
-            nodeByUri(uri: $uri) {
-              ... on Page {
-                id
-                title
-                blocks(postTemplate: false)
-              }
-            }
-          }
-        `,
-        variables: {
-            uri,
-        }
-      })
-      const blocks = cleanAndTransformBlocks (data.nodeByUri.blocks);
-      const title = data.nodeByUri.title;
-      return {
-        props: {
-          title,
-          blocks,
-        }
-      }
-}
+export const getStaticProps = getPageStaticProps;
 
 export const getStaticPaths = async () => {
     const {data} = await client.query({
--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,8 @@
-import { gql } from "@apollo/client";
-import client from "client";
 import { BlockRenderer } from "components/BlockRenderer";
 import { MainMenu } from "components/MainMenu";
-import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
-import { mapMainMenuItems } from "utils/mapMainMenuItems";
+import { Page } from "components/Page";
+import { getPageStaticProps } from "utils/getPageStaticProps";
 
-export default function Home(props) {
-  console.log("PROPS", props);
-  return (
-    <div>
-      <MainMenu 
-        items= {props.mainMenuItems} 
-        callToActionLabel={props.callToActionLabel}
-        CallToActionDestination={props.CallToActionDestination}
-      />
-      <BlockRenderer blocks={props.blocks}/>
-    </div>
-  )
-}
+export default Page;
 
-export const getStaticProps = async() => {
-  const {data} = await client.query ({
-    query: gql`
-      query PageQuery {
-        nodeByUri(uri: "/") {
-          ... on Page {
-            id
-            title
-            blocks(postTemplate: false)
-          }
-        }
-        acfOptionsMainMenu {
-          mainMenu {
-            callToActionButton {
-              label
-              destination {
-                ... on Page {
-                  uri
-                }
-              }
-            }
-            menuItems {
-              menuItem {
-                destination {
-                  ... on Page {
-                    uri
-                  }
-                }
-                label
-              }
-              items {
-                destination {
-                  ... on Page {
-                    uri
-                  }
-                }
-                label
-              }
-            }
-          }
-        }
-      }
-    `
-  })
-  const blocks = cleanAndTransformBlocks (data.nodeByUri.blocks);
-  return {
-    props: {
-      mainMenuItems: mapMainMenuItems(data.acfOptionsMainMenu.mainMenu.menuItems),
-      callToActionLabel: data.acfOptionsMainMenu.mainMenu.callToActionButton.label,
-      CallToActionDestination: data.acfOptionsMainMenu.mainMenu.callToActionButton.destination.uri,
-      blocks,
-    }
-  }
-}
+export const getStaticProps = getPageStaticProps;
