29c0ee0baee9941e6bf0e72dc96a2a429f24a666
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-06-21 15:11:49 +0800

    #56. Query the homepage, add client components, and fix Link components

--- components/FormspreeForm/FormspreeForm.js
+++ components/FormspreeForm/FormspreeForm.js
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import { ValidationError, useForm } from "@formspree/react"
 import { Input } from 'components/Input';
--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -6,5 +6,11 @@ export const Heading = ({textAlign, content, level = 2}) => {
         dangerouslySetInnerHTML: {__html: content},
         className: `font-heading max-w-5xl mx-auto my-5 ${getFontSizeForHeading(level)} ${getTextAlign(textAlign)}`
     });
+
+    // If content is undefined, return null to avoid rendering an invalid element
+    if (!content) {
+        return null;
+    }
+
     return tag;
 }
