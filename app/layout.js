f051e8c4943ba92836a68e104102c80203272988
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-06-21 15:21:46 +0800

    #57. Fix tailwind css and google fonts

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,25 @@
+import {Poppins, Aboreto} from 'next/font/google';
+
+import "../styles/globals.css";
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    display: "swap",
+    variable: "--font-poppins"
+})
+
+const aboreto = Aboreto({
+    subsets: ["latin"],
+    weight: ["400"],
+    display: "swap",
+    variable: "--font-aboreto"
+})
+
 export default function RootLayout({children}) {
     return (
-        <html lang="en">
-            <body>
+        <html lang="en" className={`${poppins.variable} ${aboreto.variable}`}>
+            <body className='font-body'>
                 {children}
             </body>
         </html>
